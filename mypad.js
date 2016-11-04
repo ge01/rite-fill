1a63b83968088176e7c9e8a37898587c064a7d4f
Gabegit config --global user.email escobedo.gabe@my.codergv.org <gabriel.escobedo01@utrgv.edu>
2016-11-04 05:08:10 -0500

    clean up

--- mypad.js
+++ mypad.js
@@ -1,30 +1,15 @@
-//module.exports = 'Hello world';
-
 module.exports = me;
 
 function me(string, padint) {
   var holdWord = [];
+
   holdWord = string.split('');
-  //console.log(holdWord);
+
   for(var i=0; i < padint; i++){
-    holdWord.push("0");
+    holdWord.push(" ");
   }
 
   stringWord = holdWord.join('');
+
   return stringWord;
-
-  //stringWord = holdWord.join('');
 }
-/*
-function leftpad (str, len, ch) {
-  //str = String(str);
-
-  var padded_string = str;
-  for(var i=0; i<len; i++){
-    padded_string += "0";
-  }
-  //callback(null, padded_string);
-
-  return str;
-}
-*/
--- testmypad.js
+++ testmypad.js
@@ -1,23 +1,3 @@
-//var msg = require('./Message.js');
-//console.log(msg);
-
 var me = require('./mypad.js');
 
 console.log(me('camel',5));
-//myPad();
-/*
-console.log(frog("hello", 5, function(err, result){
-  if(err) return console.error(err);
-  return result;
-}) + "!");
-*/
-
-/*
-module.exports = function(str, padcount, callback) {
-  var padded_string = str;
-  for(var i=0; i<padcount; i++){
-    padded_string += "0";
-  }
-  callback(null, padded_string);
-};
-*/
